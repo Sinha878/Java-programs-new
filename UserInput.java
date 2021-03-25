876d39803d5d69beaec64c02e28acfa21c3e7828
Sinha878 <64796137+Sinha878@users.noreply.github.com>
2021-03-25 11:45:01 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,20 @@
+//Program for finding the simple interest 
+package scann;
+public class UserInput {
+
+	public static void main(String[] args) 
+	{
+		int x=20;
+		int y=80;
+		int temp;
+		
+		temp=x;
+		x=y;
+		y=temp;
+		
+		System.out.println("x :"+x);
+		System.out.println("y :"+y);
+		
+	}
+}
+		
